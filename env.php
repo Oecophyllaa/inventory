86a794a310a006e0e7bc024e327761947ef9e562
Oecophyllaa <rangga.godzila@gmail.com>
2023-10-23 19:02:01 +0700

    init template

---
+++ env.php
@@ -0,0 +1,12 @@
+<?php
+$dsn = 'mysql:dbname=inventory;host=localhost';
+$user = 'root';
+$password = '';
+
+try {
+  $dbh = new PDO($dsn, $user, $password);
+  $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  // echo 'Database Connected!';
+} catch (PDOException $e) {
+  echo 'Connection Error, ' . $e->getMessage();
+}
---
+++ models/ProductIncome.php
@@ -0,0 +1,21 @@
+<?php
+
+class ProductIncome
+{
+  private $conn;
+  public function __construct()
+  {
+    global $dbh;
+    $this->conn = $dbh;
+  }
+
+  public function all()
+  {
+    $sql = "SELECT pi.*, p.name AS nama_produk, s.name AS nama_supplier, u.username, du.name AS nama_petugas FROM (((product_income pi INNER JOIN products p ON pi.product_id = p.id) INNER JOIN suppliers s ON pi.supplier_id = s.id) INNER JOIN users u ON pi.officer_id = u.id INNER JOIN detail_user du ON du.users_id = u.id);";
+    $ps = $this->conn->prepare($sql);
+    $ps->execute();
+    $res = $ps->fetchAll();
+
+    return $res;
+  }
+}
